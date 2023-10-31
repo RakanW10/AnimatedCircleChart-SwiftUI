8bc2185045eb3aa2b00439539e6a357544ab3e08
rakan <Rakan.w10@outlook.com>
2023-10-31 20:57:52 +0300

    Initial Commit

---
+++ AnimatedCircleChart/AnimatedCircleChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedCircleChartApp.swift
+//  AnimatedCircleChart
+//
+//  Created by Rakan Alotaibi on 16/04/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedCircleChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimatedCircleChart/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AnimatedCircleChart
+//
+//  Created by Rakan Alotaibi on 16/04/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
